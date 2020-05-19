d5d27eba0401591b7b3c3bf5e90c484ad71b03c3
Samuel Damasceno Filho <samuelfilho@outlook.com>
2020-05-19 03:50:55 -0300

    Desafio feito

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -20,13 +21,53 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const findProducts = await this.productsRepository.findAllById(products);
+
+    if (findProducts.length !== products.length) {
+      throw new AppError('Products not found', 400);
+    }
+
+    const orderProducts = findProducts.map(findProduct => {
+      const product = products.find(
+        productFilter => productFilter.id === findProduct.id,
+      );
+
+      if (!product || product.quantity > findProduct.quantity) {
+        throw new AppError('Products with insufficient quantities');
+      }
+
+      return {
+        product_id: findProduct.id,
+        price: findProduct.price,
+        quantity: product.quantity,
+      };
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return order;
   }
 }
 
